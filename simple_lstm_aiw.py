5e512731097636bdfb9527ea591453b73d7df0c0
simon.weiss <simon.weiss@alexanderthamm.com>
2017-11-29 19:10:58 +0100

    python file for executing on aws ec2 instance

--- simple_lstm_aiw.py
+++ simple_lstm_aiw.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import numpy as np
@@ -9,14 +9,14 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.utils import np_utils
 import sys
 
 
 # Import ASCII text of the books "Alice in Wonderland" (downloaded from project Gutenberg) and convert all characters to lowercase so that the model does not treat capitalized words as new words.
 
-# In[2]:
+# In[ ]:
 
 
 # load text and convert to lowercase
@@ -27,7 +27,7 @@ raw_text = raw_text.lower()
 
 # So that the Neural Network can use the input, each unique character is mapped to an integer value.
 
-# In[3]:
+# In[ ]:
 
 
 # create mapping of unique charst to integers
@@ -37,7 +37,7 @@ char_to_int = dict((c,i) for i, c in enumerate(chars))
 
 # Summary Statistics of the data set
 
-# In[4]:
+# In[ ]:
 
 
 n_chars = len(raw_text)
@@ -52,7 +52,7 @@ print("Total distinct characters: ", str(n_vocab))
 # 
 # The characters get converted to integers using the lookup dictionary created before.
 
-# In[5]:
+# In[ ]:
 
 
 # Prepare dataset
@@ -74,7 +74,7 @@ print("Total # of Patterns: " + str(n_patterns))
 # 2. Rescale the integers to the range 0-1 to better train the network if a sigmoid function is used (stay in approx linear part of the sigmoid). 
 # 3. One-hot encode trainY so that each character in y is represented by a vector of 45 (number of distinct characters) values. The character "n" (inter value 32) is then represented by a vector of zeros except for one "1" in column 32.
 
-# In[6]:
+# In[ ]:
 
 
 # reshape X to [samples, time steps, features]
@@ -85,103 +85,43 @@ X = X / float(n_vocab)
 y = np_utils.to_categorical(trainY)
 
 
-# I define a LSTM model with one hidden layer and 256 memory units. Dropout is used with a probability of 20%. The output layer is a Dense layer using the softmax activation function. This outputs a probability prediction for each character. These are the parameters used in the tutorial. They are not very well tuned but merely a starting point.
+# I define a LSTM model with two hidden layers and 256 memory units. Dropout is used with a probability of 20%. The output layer is a Dense layer using the softmax activation function. This outputs a probability prediction for each character. These are the parameters used in the tutorial. They are not very well tuned but merely a starting point.
 
-# In[7]:
+# In[ ]:
 
 
 model = Sequential()
-model.add(LSTM(256, input_shape = (X.shape[1], X.shape[2])))
+model.add(LSTM(256, input_shape = (X.shape[1], X.shape[2]), return_sequences = True))
+model.add(Dropout(0.2))
+model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation = "softmax"))
 model.compile(loss = "categorical_crossentropy", optimizer = "adam")
 
 
-# Next, I save the model weights after each epoch so that I can use the weights that produced the model with the lowest error for prediction afterwards. 
+# Next, I save the model weights after each epoch so that I can use the weights that produced the model with the lowest error for prediction afterwards. I also save the tensorboard callbacks to be able to see 
 
-# In[10]:
+# In[ ]:
 
 
 # define checkpoints
 filepath = "./output/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor = "loss", verbose = 1, save_best_only = True, mode = "min") 
-callbacks_list = [checkpoint]
+checkpoint = ModelCheckpoint(filepath, monitor = "loss", verbose = 1, save_best_only = True, mode = "min")
+tensorboard_cb = TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
+callbacks_list = [checkpoint, tensorboard_cb]
 
 
-# In[11]:
+# In[ ]:
 
 
-model.fit(X, y, epochs = 4, batch_size = 128, callbacks = callbacks_list)
+model.fit(X, y, epochs = 60, batch_size = 64, callbacks = callbacks_list)
 
 
-# ## Generating Text with the saved weights
-#     
-
-# In[8]:
+# In[ ]:
 
 
 # load weights
-filename = "weights-improvement-04-2.5865.hdf5"
+filename = ""
 model.load_weights(filename)
 model.compile(loss = "categorical_crossentropy", optimizer = "adam")
-
-
-# Creating a reverse mapping from integer to character to convert the predicted integers to characters.
-
-# In[9]:
-
-
-int_to_char = dict((i, c) for i, c in enumerate(chars))
-
-
-# -
-
-# In[19]:
-
-
-# return a random integer between 0 and the number of different patterns in the training data
-start = np.random.randint(0, len(trainX)-1)
-# pick the random pattern
-pattern = trainX[start]
-print("Seed: ")
-# print the random pattern by converting the integers to characater
-print("\"", "".join([int_to_char[value] for value in pattern]), "\"")
-
-
-# Printing 1000 characters of generated text starting with the random pattern above:
-
-# In[20]:
-
-
-# initialize empty list for the result_output
-result_output = []
-for i in range(1000):
-    x = np.reshape(pattern, (1, len(pattern), 1))
-    x = x / float(n_vocab)
-    # prediction contains the probability for each character (0-45) for the given input pattern x
-    prediction = model.predict(x, verbose = 0)
-    # index contains the index where the prediction is highest
-    index = np.argmax(prediction)
-    # the predicted character
-    result = int_to_char[index]
-    # the input sequence 
-    seq_in = [int_to_char[value] for value in pattern]
-    # write to console
-    sys.stdout.write(result)
-    # append predicted index to the result_output list
-    result_output.append(index)
-    # append predicted index to the pattern
-    pattern.append(index)
-    # new pattern is the old pattern with the first character cut away and the new prediction appended to the end. this new pattern is the input for the next iteration
-    pattern = pattern[1:len(pattern)]
-
-
-# Print the output and wirte to txt
-
-# In[29]:
-
-
-with open("./output/prediction.txt", "w") as f:
-    f.write("".join([int_to_char[value] for value in result_output]))
-    print("done")
 
